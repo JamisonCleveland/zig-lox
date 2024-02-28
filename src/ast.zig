b9f713b50c8a333d2a64a5f1d6ab58fb8ebf5cbe
JamisonCleveland <jamisonc1128@gmail.com>
2024-02-28 14:54:11 -0700

    lox value parsing

--- src/ast.zig
+++ src/ast.zig
@@ -1,17 +1,19 @@
 const std = @import("std");
 const Token = @import("lexer.zig").Token;
 
-const Literal = union(Tag) {
-    const Tag = enum { number };
+const LoxValue = union(Tag) {
+    const Tag = enum { number, string, bool, nil };
     number: f32,
-    //string: [:0]const u8,
+    string: []const u8,
+    bool: bool,
+    nil: struct {},
 };
 
 const Expr = union(Tag) {
     const Self = @This();
     binary: Binary,
     grouping: Grouping,
-    literal: Literal,
+    literal: LoxValue,
     unary: Unary,
     const Tag = enum { binary, grouping, literal, unary };
     const Binary = struct {
@@ -31,7 +33,7 @@ const Expr = union(Tag) {
 fn ast_print(expr: *const Expr, writer: anytype) !void {
     switch (expr.*) {
         .binary => |b| {
-            _ = try writer.write("(+ ");
+            try writer.print("({s} ", .{b.operator.lexeme});
             try ast_print(b.left, writer);
             _ = try writer.write(" ");
             try ast_print(b.right, writer);
@@ -46,22 +48,60 @@ fn ast_print(expr: *const Expr, writer: anytype) !void {
             _ = try std.fmt.format(writer, "{d}", .{l.number});
         },
         .unary => |u| {
-            _ = try writer.write("(- ");
+            try writer.print("({s} ", .{u.operator.lexeme});
             try ast_print(u.right, writer);
             _ = try writer.write(")");
         },
     }
 }
 
+const Parser = struct {
+    const Self = @This();
+    tokens: []Token,
+    pos: usize,
+
+    fn parseLoxValue(p: *Self) !LoxValue {
+        const t = p.tokens[p.pos];
+        switch (t.tag) {
+            .NUMBER => {
+                p.pos += 1;
+                const n = std.fmt.parseFloat(f32, t.lexeme) catch unreachable;
+                return LoxValue{ .number = n };
+            },
+            .STRING => {
+                p.pos += 1;
+                return LoxValue{ .string = t.lexeme[1 .. t.lexeme.len - 1] };
+            },
+            .TRUE, .FALSE => {
+                p.pos += 1;
+                return LoxValue{ .bool = t.tag == .TRUE };
+            },
+            .NIL => {
+                p.pos += 1;
+                return LoxValue{ .nil = .{} };
+            },
+            else => return error.UnexpectedToken,
+        }
+    }
+};
+
 test "asdf" {
-    var lit1 = Expr{ .literal = Literal{ .number = 2.0 } };
-    var lit2 = Expr{ .literal = Literal{ .number = 2.0 } };
-    var add1 = Expr{ .binary = Expr.Binary{
-        .left = &lit1,
-        .operator = .{ .tag = .PLUS, .loc = undefined },
-        .right = &lit2,
-    } };
-    const stdout = std.io.getStdOut().writer();
-    try ast_print(&add1, stdout);
-    _ = try stdout.write("\n");
+    var toks = [4]Token{
+        .{ .tag = Token.Tag.NUMBER, .lexeme = "123", .loc = undefined },
+        .{ .tag = Token.Tag.STRING, .lexeme = "\"asdf\"", .loc = undefined },
+        .{ .tag = Token.Tag.TRUE, .lexeme = "true", .loc = undefined },
+        .{ .tag = Token.Tag.NIL, .lexeme = "nil", .loc = undefined },
+        .{ .tag = Token.Tag.PLUS, .lexeme = "nil", .loc = undefined },
+    };
+    var p = Parser{ .tokens = &toks, .pos = 0 };
+    const a = try p.parseLoxValue();
+    const b = try p.parseLoxValue();
+    const c = try p.parseLoxValue();
+    const d = try p.parseLoxValue();
+    const e = try p.parseLoxValue();
+    std.debug.print("{d}\n", .{a.number});
+    std.debug.print("'{s}'\n", .{b.string});
+    std.debug.print("{?}\n", .{c});
+    std.debug.print("{?}\n", .{d});
+    std.debug.print("{?}\n", .{e});
 }
